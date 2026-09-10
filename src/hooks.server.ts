6021320a4ba3296dea0840dc5ce2dc928f65b793
firmanizzani <firman.izzanifrc@gmail.com>
2026-09-11 00:20:19 +0700

    chore: remove unused server script and cleanup imports

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,3 @@
-import { redirect } from '@sveltejs/kit';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
--- src/server/server.ts
+++
@@ -1,4 +0,0 @@
-import { app } from './index.js';
-
-app.listen(3000);
-console.log(`🚀 Server Elysia.js POS running at http://${app.server?.hostname}:${app.server?.port}`);
